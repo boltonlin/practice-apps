60bdb3f858c595db75555b82f0b908e9b8bbfee8
boltonlin <boltonlinbo@gmail.com>
2023-01-19 10:46:13 -0800

    defined db functions

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,9 +1,29 @@
 const mongoose = require("mongoose");
 
-// 2. Set up any schema and models needed by the app
-// 3. Export the models
-// 4. Import the models into any modules that need them
-
 mongoose.connect(`mongodb://localhost/${process.env.DB_NAME}`)
   .then(() => console.log(`Connected to ${process.env.DB_NAME}`))
   .catch((err) => console.log('Error connecting'));
+
+// automatic _id, homonyms exist
+const termSchema = mongoose.Schema({
+  word:         String,
+  definition:   String,
+});
+
+const Term = mongoose.model('Term', termSchema);
+
+module.exports = {
+
+  saveTerm: function (term) {
+
+  },
+
+  retrieveTerm: function (word) {
+
+  },
+
+  updateTerm: function (word, newDefinition) {
+
+  },
+
+};
