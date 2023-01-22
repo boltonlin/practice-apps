262e3cb1c70fcc5c59953fa9005139115f67ee38
boltonlin <boltonlinbo@gmail.com>
2023-01-21 16:39:01 -0800

    added login route and handler

--- 2-checkout/server/controllers.js
+++ 2-checkout/server/controllers.js
@@ -39,11 +39,47 @@ module.exports = {
       default:
         res.sendStatus(201);
     }
-
   },
 
   fetch: function (req, res) {
+    const { type, payload } = req.body;
+    switch (type) {
+      case 'user':
+        return db
+          .find(type, payload)
+          .then(results => res.status(200).send(results[0]))
+          .catch(err => res.status(400).send(err));
+        break;
+      case 'address':
+      case 'payment':
+        return db
+          .findSession(req.session_id)
+          .then(results => {
+            if (!results[0].length) res.sendStatus(400);
+            payload.user_id = results[0][0].user_id;
+            return db.findByUserId(type, payload);
+          })
+          .then(results => res.status(200).send(results[0]))
+          .catch(err => res.status(400).send(err));
+        break;
+      default:
+        res.sendStatus(201);
+    }
+  },
 
+  login: function (req, res) {
+    const { account, password: attempt } = req.body;
+    let user_id;
+    return db
+      .matchUser(account, attempt)
+      .then(results => {
+        if (!results[0].length) res.sendStatus(400);
+        user_id = results[0][0].id;
+        return db.clearSessionByUserId(user_id);
+      })
+      .then(() => db.createSession(req.session_id, user_id))
+      .then(res.sendStatus.bind(res, 201))
+      .catch(err => res.status(400).send(err));
   },
 
   logout: function (req, res) {
--- 2-checkout/server/routes.js
+++ 2-checkout/server/routes.js
@@ -3,6 +3,7 @@ const controller = require("./controllers");
 
 router.put('/forms', controller.createOrUpdate);
 router.get('/forms', controller.fetch);
+router.post('/login', controller.login);
 router.get('/logout', controller.logout);
 
 module.exports = router;
